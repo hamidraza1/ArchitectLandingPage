6fb0578ea3b9cfc53d2fd052b30e58409f452ec5
hamidraza1 <hrazaa306@gmail.com>
2020-07-04 17:23:50 +0200

    Updates

---
+++ precache-manifest.e7a5061cd4fae16bb92c63b85f461f33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff68751f65e81f8a3d99d294a32c54f",
+    "url": "/ArchitectLandingPage/index.html"
+  },
+  {
+    "revision": "f7a0f28b14adcc35f550",
+    "url": "/ArchitectLandingPage/static/css/main.82a43c74.chunk.css"
+  },
+  {
+    "revision": "e7cda8d43a0032ef7fca",
+    "url": "/ArchitectLandingPage/static/js/2.cb4246e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ArchitectLandingPage/static/js/2.cb4246e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a0f28b14adcc35f550",
+    "url": "/ArchitectLandingPage/static/js/main.3327b10f.chunk.js"
+  },
+  {
+    "revision": "a96fb76a72ce08301101",
+    "url": "/ArchitectLandingPage/static/js/runtime-main.239f154f.js"
+  }
+]);
